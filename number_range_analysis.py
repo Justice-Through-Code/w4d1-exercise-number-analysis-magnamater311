59ba70e299df0079637d9dc137fba158c2ed108b
Philosophers Fuel <donsells3535@gmail.com>
2023-09-30 13:25:41 -0700

    Chugga Chugga

--- number_range_analysis.py
+++ number_range_analysis.py
@@ -24,11 +24,29 @@ def calculate_sum(start, end):
         int: The sum of numbers within the range.
     """
     # TODO: Implement the logic to calculate the sum of numbers within the range.
+    sum = 0
+    for num in range(start, end + 1):
+        sum += num
+
+    return sum
+        
+        
+
+
     # TODO: Return the calculated sum.
 
 def find_smallest_number(start, end):
+
+    smallest_number = 100000000
+    for num in range(start, end + 1):
+        if num < smallest_number:
+            smallest_number = num
+
+    return smallest_number
+
     """
-    Find the smallest number within the specified range.
+    Find the smallest number within the specified 
+    range.
 
     Arguments:
         start (int): The starting number of the range (inclusive).
@@ -41,6 +59,14 @@ def find_smallest_number(start, end):
     # TODO: Return the found smallest number.
 
 def find_largest_number(start, end):
+
+    largest_number = -10000000
+    for num in range(start, end + 1):
+        if num > largest_number:
+            largest_number = num
+
+    return largest_number
+
     """
     Find the largest number within the specified range.
 
@@ -55,6 +81,18 @@ def find_largest_number(start, end):
     # TODO: Return the found largest number.
 
 def count_even_numbers(start, end):
+
+    count = 0
+
+    for num in range(start, end + 1):
+        # if number is even count + 1
+        
+        if num % 2 == 0:
+            count += 1 
+
+        
+    return count
+
     """
     Count the number of even numbers within the specified range.
 
@@ -69,6 +107,16 @@ def count_even_numbers(start, end):
     # TODO: Return the count of even numbers.
 
 def count_odd_numbers(start, end):
+    count = 0
+
+    for num in range(start, end + 1):
+        # if number is odd count + 1
+        
+        if num % 2 != 0:
+            count += 1 
+
+        
+    return count
     """
     Count the number of odd numbers within the specified range.
 
